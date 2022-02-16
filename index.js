680a794dada1edd58e83b02e4c8cd6aec4f87318
Eduardo Moreira <contato@eduardoem.com.br>
2022-02-16 00:53:40 -0300

    Update script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotEnv from 'dotenv'
 import AuthBffClient from '@cig-platform/auth-bff-client'
 import BackofficeBffClient from '@cig-platform/backoffice-bff-client'
 import { breederFactory, poultryFactory, userFactory } from '@cig-platform/factories'
-import { PoultryColorEnum, PoultryGenderCategoryEnum, RegisterTypeEnum } from '@cig-platform/enums'
+import { PoultryColorEnum, PoultryGenderCategoryEnum, PoultryGenderEnum, RegisterTypeEnum } from '@cig-platform/enums'
 
 dotEnv.config()
 
@@ -17,14 +17,6 @@ const POULTRY_AMOUNT_PER_GENDER = 1;
 
 const authBffClient = new AuthBffClient.default(AUTH_BFF_URL)
 const backofficeBffClient = new BackofficeBffClient.default(BACKOFFICE_BFF_URL)
-
-const sleep = (milliseconds) => {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
 
 const randomCode = () => {
   const codeLength = 4;
@@ -41,9 +33,198 @@ const randomCode = () => {
 }
 
 const PoultriesFactory = async (user, breeder, token) => {
+  console.log('Generating females chickens')
   await Promise.all(Array(POULTRY_AMOUNT_PER_GENDER).fill({}).map(async () => {
     const poultry = poultryFactory({
       genderCategory: PoultryGenderCategoryEnum.FemaleChicken,
+      colors: {
+        eyes: PoultryColorEnum.BLACK,
+        plumage: PoultryColorEnum.BLACK,
+        shins: PoultryColorEnum.BLACK
+      }
+    })
+
+    delete poultry['active']
+    delete poultry['id']
+    delete poultry['number']
+    delete poultry['videos']
+    delete poultry['description']
+    delete poultry['birthDate']
+    delete poultry['register']
+
+    const poultryResponseData = await backofficeBffClient.postPoultry(
+      breeder.id,
+      token,
+      poultry,
+    )
+
+    await backofficeBffClient.postRegister(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        type: RegisterTypeEnum.MeasurementAndWeighing,
+        description: 'Medição de hoje!',
+        metadata: { weight: 100, measurement: 150 },
+        date: new Date()
+      }
+    )
+
+    await backofficeBffClient.postRegister(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        type: RegisterTypeEnum.Vaccination,
+        description: 'Vacinação de hoje!',
+        metadata: { name: 'Pfizer', dose: 1 },
+        date: new Date()
+      }
+    )
+
+    await backofficeBffClient.postAdvertising(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        externalId: poultryResponseData.poultry.id,
+        price: 150
+      }
+    )
+
+    return null
+  }))
+
+  console.log('Generating females matrixes')
+  await Promise.all(Array(POULTRY_AMOUNT_PER_GENDER).fill({}).map(async () => {
+    const poultry = poultryFactory({
+      genderCategory: PoultryGenderCategoryEnum.Matrix,
+      colors: {
+        eyes: PoultryColorEnum.BLACK,
+        plumage: PoultryColorEnum.BLACK,
+        shins: PoultryColorEnum.BLACK
+      }
+    })
+
+    delete poultry['active']
+    delete poultry['id']
+    delete poultry['number']
+    delete poultry['videos']
+    delete poultry['description']
+    delete poultry['birthDate']
+    delete poultry['register']
+
+    const poultryResponseData = await backofficeBffClient.postPoultry(
+      breeder.id,
+      token,
+      poultry,
+    )
+
+    await backofficeBffClient.postRegister(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        type: RegisterTypeEnum.MeasurementAndWeighing,
+        description: 'Medição de hoje!',
+        metadata: { weight: 100, measurement: 150 },
+        date: new Date()
+      }
+    )
+
+    await backofficeBffClient.postRegister(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        type: RegisterTypeEnum.Vaccination,
+        description: 'Vacinação de hoje!',
+        metadata: { name: 'Pfizer', dose: 1 },
+        date: new Date()
+      }
+    )
+
+    await backofficeBffClient.postAdvertising(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        externalId: poultryResponseData.poultry.id,
+        price: 150
+      }
+    )
+
+    return null
+  }))
+
+  console.log('Generating male chickens')
+  await Promise.all(Array(POULTRY_AMOUNT_PER_GENDER).fill({}).map(async () => {
+    const poultry = poultryFactory({
+      gender: PoultryGenderEnum.Male,
+      genderCategory: PoultryGenderCategoryEnum.MaleChicken,
+      colors: {
+        eyes: PoultryColorEnum.BLACK,
+        plumage: PoultryColorEnum.BLACK,
+        shins: PoultryColorEnum.BLACK
+      }
+    })
+
+    delete poultry['active']
+    delete poultry['id']
+    delete poultry['number']
+    delete poultry['videos']
+    delete poultry['description']
+    delete poultry['birthDate']
+    delete poultry['register']
+
+    const poultryResponseData = await backofficeBffClient.postPoultry(
+      breeder.id,
+      token,
+      poultry,
+    )
+
+    await backofficeBffClient.postRegister(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        type: RegisterTypeEnum.MeasurementAndWeighing,
+        description: 'Medição de hoje!',
+        metadata: { weight: 100, measurement: 150 },
+        date: new Date()
+      }
+    )
+
+    await backofficeBffClient.postRegister(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        type: RegisterTypeEnum.Vaccination,
+        description: 'Vacinação de hoje!',
+        metadata: { name: 'Pfizer', dose: 1 },
+        date: new Date()
+      }
+    )
+
+    await backofficeBffClient.postAdvertising(
+      breeder.id,
+      poultryResponseData.poultry.id,
+      token,
+      {
+        externalId: poultryResponseData.poultry.id,
+        price: 150
+      }
+    )
+
+    return null
+  }))
+
+  console.log('Generating male reproductives')
+  await Promise.all(Array(POULTRY_AMOUNT_PER_GENDER).fill({}).map(async () => {
+    const poultry = poultryFactory({
+      gender: PoultryGenderEnum.Male,
+      genderCategory: PoultryGenderCategoryEnum.Reproductive,
       colors: {
         eyes: PoultryColorEnum.BLACK,
         plumage: PoultryColorEnum.BLACK,
